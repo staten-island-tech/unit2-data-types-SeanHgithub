35acd1a4194c3e6ee4af6f7ab3bc7893f3e39b9a
SeanHgithub <seanhadley11@gmail.com>
2025-09-15 10:18:59 -0400

    unit2

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 """ x = 3
 y = float(3)
 print(x,y) """
-values = [1,2.23,5,7,2,30,15]
+""" values = [1,2.23,5,7,2,30,15] """
 """ print(values)
 for i in values:
     print(i) """
 """ print(values[0])
 print(values[6]) """
 """ print(values[7]) """
-x = "this is a thing"
+""" x = "this is a thing"
 y = x.split( )
 z = y[0]
 print(y)
-print(z)
+print(z) """
+""" day_of_week = input("what day is it?")
+if day_of_week == "Monday":
+    print("correct")
+else:
+    print("incorrect") """
+""" x = "test"
+print(f"hello {x}") """
+""" temp = 75
+if temp > 68:
+    print("warm")
+elif temp == 68:
+    print("perfect")
+else:
+    print("cold") """
+def odd_even(x):
+    if x % 2 == 0:
+        return("even")
+    else:
+        return ("odd")
+values = [1,2,3,4,5,6,7,8,9,10]
